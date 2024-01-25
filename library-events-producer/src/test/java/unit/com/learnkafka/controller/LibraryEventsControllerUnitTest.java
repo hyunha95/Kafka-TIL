cae57a078477f4bef1b3ed812d9818281b5a82b4
nohyunha <nohyunha95@gmail.com>
2024-01-25 22:17:45 +0900

    카프카 단위 테스트

---
+++ library-events-producer/src/test/java/unit/com/learnkafka/controller/LibraryEventsControllerUnitTest.java
@@ -0,0 +1,68 @@
+package com.learnkafka.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.producer.LibraryEventsProducer;
+import com.learnkafka.util.TestUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// Test Slice
+@WebMvcTest(LibraryEventsController.class)
+class LibraryEventsControllerUnitTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    LibraryEventsProducer libraryEventsProducer;
+
+    @Test
+    void postLibraryEvent() throws Exception {
+        // given
+        var json = objectMapper.writeValueAsString(TestUtil.libraryEventRecord());
+        when(libraryEventsProducer.sendLibraryEvent_approach3(isA(LibraryEvent.class)))
+                .thenReturn(null);
+
+        // when
+        mockMvc.perform(post("/v1/libraryevent")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void postLibraryEvent_4xx() throws Exception {
+        // given
+        var json = objectMapper.writeValueAsString(TestUtil.libraryEventRecordWithInvalidBook());
+        when(libraryEventsProducer.sendLibraryEvent_approach3(isA(LibraryEvent.class)))
+                .thenReturn(null);
+
+        var expectedErrorMessage = "book.bookId - must not be null, book.bookName - must not be blank";
+
+        // when
+        mockMvc.perform(post("/v1/libraryevent")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().string(expectedErrorMessage));
+    }
+}
