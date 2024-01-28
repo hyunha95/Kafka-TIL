5c2b7a1c980eb531d2701d3fe7f562fe9d04eeec
nohyunha <nohyunha95@gmail.com>
2024-01-28 21:01:42 +0900

    소비자 그룹 및 소비자 오프셋 관

---
+++ library-events-consumer/src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,24 @@
+package com.learnkafka.config;
+
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+
+import static org.springframework.kafka.listener.ContainerProperties.AckMode.*;
+
+@Configuration
+//@EnableKafka
+public class LibraryEventsConsumerConfig {
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ConsumerFactory<Object, Object> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory);
+//        factory.getContainerProperties().setAckMode(MANUAL);
+        return factory;
+    }
+}
